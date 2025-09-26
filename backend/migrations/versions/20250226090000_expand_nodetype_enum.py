c4d377c5266c4790b83c60b0be3ef1e003d70851
MilesAheadToo <trevor.miles@azirella.com>
2025-09-26 11:29:58 +0000

    20250926 12:30

---
+++ backend/migrations/versions/20250226090000_expand_nodetype_enum.py
@@ -0,0 +1,44 @@
+"""Expand nodetype enum to cover market nodes
+
+Revision ID: 20250226090000
+Revises: 20241101090000
+Create Date: 2025-09-26 09:00:00.000000
+
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "20250226090000"
+down_revision = "20241101090000"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute(
+        """
+        ALTER TABLE nodes
+        MODIFY COLUMN type ENUM(
+            'RETAILER',
+            'WHOLESALER',
+            'DISTRIBUTOR',
+            'MANUFACTURER',
+            'MARKET_SUPPLY',
+            'MARKET_DEMAND'
+        ) NOT NULL
+        """
+    )
+
+
+def downgrade() -> None:
+    op.execute(
+        """
+        ALTER TABLE nodes
+        MODIFY COLUMN type ENUM(
+            'RETAILER',
+            'WHOLESALER',
+            'DISTRIBUTOR',
+            'MANUFACTURER'
+        ) NOT NULL
+        """
+    )
